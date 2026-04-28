f17ce13f12ac9107806f0964d086d055019c853d
rajeshwaranx-dev <115334651+rajeshwaranx-dev@users.noreply.github.com>
2026-04-28 18:06:54 +0530

    Ask Botz❤️‍🔥

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ DB_NAME = os.environ.get("DB_NAME", "bharath")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "50"))
 # Message displayed to users when they click the Start button or send the /start command.
 
-START_MSG = os.environ.get("START_MESSAGE", "⚠️ This is a File Store Bot\n\nYou cannot search movies here.\n\n🎬 To download movies, go to our channel and check the channel Links.\n\n📢 Channel: https://t.me/Tamilmov_Linkz")
+START_MSG = os.environ.get("START_MESSAGE", "⚠️ This is a File Store Bot\n\nYou cannot search movies here.\n\n🎬 To download movies, go to our channel and check the channel Links.\n\n📢 Channel: https://t.me/TamilEvo_Linkz")
 OWNER_TAG = os.environ.get("OWNER_TAG", "the_universal_being")
 # Time (in seconds) after which messages will be auto-deleted. Use 0 to keep messages permanently.
 TIME = int(os.environ.get("TIME", "600"))
@@ -56,7 +56,7 @@ USER_REPLY_TEXT = os.environ.get(
     "⚠️ This is a File Store Bot\n\n"
     "You cannot search movies here.\n\n"
     "🎬 To download movies, go to our channel and check the channel Links.\n\n"
-    "📢 Channel: https://t.me/Tamilmov_Linkz"
+    "📢 Channel: https://t.me/TamilEvo_Linkz"
 )
 
 # Toggle this feature by setting the value inside to either True (on) or False (off).
